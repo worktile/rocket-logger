df92ed0ab9796c08f49040f06148390c7c36003e
zhaoli <zhaoli@worktile.com>
2021-07-05 10:22:26 +0800

    fix: remove index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,40 @@
+import { FileTransportOptions, ConsoleTransportOptions } from "winston";
+import { ClientOptions } from "@elastic/elasticsearch";
+
+declare class RocketLogger {
+    constructor(options?: RocketLogger.RocketLoggerOptions): RocketLogger;
+
+    log(level: string, message: string | Error | object, meta?: object | string): void;
+
+    error(message: string | Error, meta?: Error | object | string): void;
+
+    debug(message: string, meta?: object): void;
+
+    info(message: string, meta?: object): void;
+
+    warn(message: string, meta?: object): void;
+}
+
+export = RocketLogger;
+
+declare namespace RocketLogger {
+    export interface ExtendFileTransportOptions extends FileTransportOptions {
+        errorFileName?: string
+    }
+    
+    export interface MongoDBTransportOptions {
+        mongodbServer: string;
+        collection: string;
+        errorCollection: string;
+        mongodbOptions: any;
+        label: string;
+    }
+    
+    export interface RocketLoggerOptions {
+        file?: ExtendFileTransportOptions | boolean;
+        es?: ClientOptions;
+        console?: ConsoleTransportOptions | boolean;
+    }
+    
+}
+
